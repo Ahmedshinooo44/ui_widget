a9d632d32d243e0d8979a2ebdc71898c7d6f09b9
ahmedshinooo44 <ahmedshinooo44@gmail.com>
2024-07-14 05:44:26 +0300

    ui widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/safe_area.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    Scaffold(
-      appBar: AppBar(
-        title: const Text("Ms Shinoo"),
-        backgroundColor: const Color.fromARGB(255, 54, 39, 189),
-      ),
-    ),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 89, 157, 212),
+        body: SafeArea(
+          top: true,
+          right: true,
+          left: true,
+          bottom: true,
+          child: Column(
+            verticalDirection: VerticalDirection.down,
+            children: [
+              Card(
+                color: Color.fromARGB(38, 8, 0, 116),
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                child: SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Center(
+                    child: Text("AHmed"),
+                  ),
+                ),
+              ),
+              Card(
+                color: Color.fromARGB(38, 8, 0, 116),
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                child: SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Center(
+                    child: Text("AHmed"),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Card(
+                    color: Color.fromARGB(36, 83, 78, 146),
+                    margin: EdgeInsets.symmetric(
+                        vertical: 20 /*الطول */, horizontal: 30 /*العرض */),
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Center(
+                        child: Text("shino"),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color.fromARGB(36, 83, 78, 146),
+                    margin: EdgeInsets.symmetric(
+                        vertical: 20 /*الطول */, horizontal: 20 /*العرض */),
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Center(
+                        child: Text("shino"),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
